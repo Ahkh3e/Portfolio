9ad9dffe62ca76d7c28478d5332c128bb710715c
Muhammad Khan <mahmedkhan19@gmail.com>
2020-07-25 18:41:56 -0400

    Plots are succesfully placed on a single grid with selectable option

--- CovidDashboard/dashboard.py
+++ CovidDashboard/dashboard.py
@@ -3,55 +3,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from datetime import datetime, timedelta
-
-
+from numpy import empty
 @st.cache
 def fetch_data():
     df = pd.read_json('https://covidtracking.com/api/v1/us/daily.json')
     df['date'] = pd.to_datetime(df['date'], format="%Y%m%d")
     df.set_index('date', inplace=True)
     df.sort_index(ascending=True, inplace=True)
-
     return df
-
-def data():
-    return pd.DataFrame(df, options)
-
-
-
-options = {"Cumulative Positive Results": 'positive',
-    "Daily Positive Tests": 'positiveIncrease',
-    "Cumluative Deaths": 'death',
-    "Daily Deaths": 'deathIncrease',
-    "Current Hospitalizations": 'hospitalizedCurrently',
-    "Daily Hospitalizations": 'hospitalizedIncrease',
-    "Cumulative Hospitalizations": 'hospitalizedCumulative',
-    "Current ICU Patients": 'inIcuCurrently',
-    "Cumulative ICU Patients": 'inIcuCumulative',
-    "Current Ventilator Patients": 'onVentilatorCurrently',
-    "Cumulative Ventilator Patients": 'onVentilatorCumulative',
-    "Recovered Patients": 'recovered',
-    "Daily Tests Performed": 'totalTestResultsIncrease',
-    "Cumulative Tests Performed": 'totalTestResults'}
-
-
+def data(charts):
+    return pd.DataFrame(df, charts)
+options = ['positive',
+    'positiveIncrease',
+    'death',
+    'deathIncrease',
+    'hospitalizedCurrently',
+'hospitalizedIncrease',
+    'hospitalizedCumulative',
+    'inIcuCurrently',
+    'inIcuCumulative',
+'onVentilatorCurrently',
+    'onVentilatorCumulative',
+'recovered',
+'totalTestResultsIncrease',
+    'totalTestResults']
 df = fetch_data()
-
     ## Build page
 st.title('COVID-19 Dashboard')
 st.header('US data')
 st.subheader('Source: https://covidtracking.com')
-
-
 start_date = st.sidebar.date_input("Start Date", value=datetime(2020,3,1))
 end_date = st.sidebar.date_input("End Date", value=df.index.max())
 charts = st.sidebar.multiselect("Select individual charts to display:",
-                options=list(options.keys()),
-                default=list(options.keys())[0:1])
-
-
-for chart in charts:
-    print(options[chart])
-    st.area_chart(df[options[chart]])
-
+                options=list(options))
+st.area_chart(df[charts])
 st.pyplot()
